6d6d94137333d419b583ebc89aee5440725a8eed
mvpetrungaro <marcos.petrungaro@gmail.com>
2020-05-26 15:53:18 -0300

    Update install_node.sh

--- contrib/etc/install_node.sh
+++ contrib/etc/install_node.sh
@@ -2,22 +2,25 @@
 
 set -ex
 
-#yum install -y git
+yum install -y git
 
 # Ensure git uses https instead of ssh for NPM install
 # See: https://github.com/npm/npm/issues/5257
-#echo -e "Setting git config rules"
-#git config --system url."https://github.com".insteadOf git@github.com:
-#git config --global url."https://github.com".insteadOf ssh://git@github.com
-#git config --system url."https://".insteadOf git://
-#git config --system url."https://".insteadOf ssh://
-#git config --list
+echo -e "Setting git config rules"
+git config --system url."https://github.com".insteadOf git@github.com:
+git config --global url."https://github.com".insteadOf ssh://git@github.com
+git config --system url."https://".insteadOf git://
+git config --system url."https://".insteadOf ssh://
+git config --list
 
-yum install -y nodejs
-which node
-which nodejs
+#yum install -y nodejs
+#which node
+#which nodejs
 
-#rpm -V $INSTALL_PKGS
+yum install -y https://github.com/nodeshift/node-rpm/releases/download/v${NODE_VERSION}/rhoar-nodejs-${NODE_VERSION}-1.el7.centos.x86_64.rpm
+yum install -y https://github.com/nodeshift/node-rpm/releases/download/v${NODE_VERSION}/npm-${NPM_VERSION}-1.${NODE_VERSION}.1.el7.centos.x86_64.rpm
+
+rpm -V $INSTALL_PKGS
 yum clean all -y
 ldconfig
 
@@ -31,7 +34,7 @@ echo "---> Setting directory write permissions"
 fix-permissions /opt/app-root
 
 # Delete NPM things that we don't really need (like tests) from node_modules
-#find /usr/local/lib/node_modules/npm -name test -o -name .bin -type d | xargs rm -rf
+find /usr/local/lib/node_modules/npm -name test -o -name .bin -type d | xargs rm -rf
 
 # Clean up the stuff we downloaded
 yum clean all -y
